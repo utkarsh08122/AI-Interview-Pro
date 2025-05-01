bbd338080446e3635eb7b1bdf464b7cc4460316a
utkarshsingh <utkarshsingh08122002@gmail.com>
2025-05-01 11:49:07 +0530

    Update localStorageManager.ts

--- src/lib/localStorageManager.ts
+++ src/lib/localStorageManager.ts
@@ -1,1 +1,11 @@
+"use client";
+export const KEY_ACCESS_TOKEN = "RefresToken";
+
+export function getItem(key: any) {
+  return localStorage.getItem(key);
+}
+
+export function setItem(key: any, value: any) {
+  localStorage.setItem(key, value);
+}
 
