0f1d9dddcc10613e290bc5e98fb9382903316ea7
utkarshsingh <utkarshsingh08122002@gmail.com>
2025-05-01 11:49:33 +0530

    Update axiosInstance.ts

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,11 +1,15 @@
 "use client";
 import axios from "axios";
+import { KEY_ACCESS_TOKEN, getItem } from "./localStorageManager";
 
 export const axiosClient = axios.create({
-  baseURL:process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
 });
-// export const axiosClient = axios.create({
-//   baseURL: "http://localhost:3000",
-//   withCredentials: true,
-// });
+
+axiosClient.interceptors.request.use((request) => {
+  const RefresToken = getItem(KEY_ACCESS_TOKEN);
+  request.headers["Authorization"] = `Bearer ${RefresToken}`;
+  return request;
+});
+
