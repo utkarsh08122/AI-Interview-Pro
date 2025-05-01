80819195a0433069548e97b368f9ea676956c02e
utkarshsingh <utkarshsingh08122002@gmail.com>
2025-05-01 17:59:32 +0530

    Update page.tsx

--- src/app/(root)/interview/page.tsx
+++ src/app/(root)/interview/page.tsx
@@ -1,16 +1,13 @@
-import Agent from "@/components/Agent";
+import InterviewForm from "@/components/InterviewForm";
 import { MyCookiesComponent } from "@/helper/Token";
 
 const Page = async () => {
   const { name, id }: any = await MyCookiesComponent();
-  console.log(name, id);
-
 
   return (
-    <>
-      <h3>Interview generation</h3>
-      <Agent key={id} userName={name} userId={id} type="generate" />
-    </>
+    <div className="flex justify-center">
+        <InterviewForm key={id} userId={id}/>
+    </div>
   );
 };
 
