be554be052e7fddab795243a5b7b589aee25343e
utkarshsingh <utkarshsingh08122002@gmail.com>
2025-05-01 10:33:02 +0530

    Update route.ts

--- src/app/api/vapi/generate/route.ts
+++ src/app/api/vapi/generate/route.ts
@@ -15,12 +15,6 @@ export async function POST(req: NextRequest) {
   const { type, role, level, techstack, amount,userid} =await req.json();
   
   try {
-  const RefresToken =await request.cookies.get("RefresToken", {
-      httpOnly: true,
-      secure: true,
-    })?.value || "";
-    
-    const{id,name}:any=jwtDecode(RefresToken);
     const genAi = new GoogleGenAI({
       apiKey:process.env.GOOGLE_GENERATIVE_AI_API_KEY,
     });
@@ -48,7 +42,7 @@ export async function POST(req: NextRequest) {
       level,
       techstack: techstack.split(","),
       questions: JSON.parse(questions),
-      userId:id,
+      userId:userid,
       finalized: true,
     };
     console.log("1", interview);
