dd44f381989e88a28334107dd12b1114517c7b7d
utkarshsingh <utkarshsingh08122002@gmail.com>
2025-05-02 16:38:54 +0530

    Update route.ts

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,1 +1,13 @@
 
+import { NextRequest, NextResponse } from "next/server";
+import { success, error } from "@/helper/responsController";
+import { cookies } from "next/headers";
+export async function DELETE(req: NextRequest) {
+  try {
+    const token = await cookies();
+    token.delete("RefresToken");
+    return NextResponse.json(success(200, "success"));
+  } catch (e: any) {
+    return NextResponse.json(error(400, e._message));
+  }
+}
